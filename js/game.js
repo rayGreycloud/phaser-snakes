74ac62e73aa7a782ad1062e1085c2c90612a5aa6
rayGreycloud <rayGreycloud@gmail.com>
2017-07-05 14:26:52 -0700

    Add game skeleton

---
+++ js/game.js
@@ -0,0 +1,19 @@
+Game = function (game) {}
+
+Game.prototype = {
+  preload: function () {
+    // Load assets
+
+  },
+  create: function () {
+    // Initialize world
+
+    // Initialize physics and groups
+
+    // Create player
+  },
+  update: function () {
+    // Update game components
+
+  }
+};
